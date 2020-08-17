33483a38eae7aa288bffe7288cccbe7c9ce6937c
王垚燊(Yaoshen Wang) <wangyaoshen@genomics.cn>
2020-08-17 10:55:08 +0800

    add Slice2MapMapArray

--- simpleUtil/util.go
+++ simpleUtil/util.go
@@ -42,3 +42,32 @@ func Slice2MapArray(slice [][]string) (db []map[string]string) {
 	}
 	return
 }
+
+func Slice2MapMapArray(slice [][]string, key string) map[string]map[string]string {
+	var db = make(map[string]map[string]string)
+	var title []string
+	for i, array := range slice {
+		if i == 0 {
+			title = array
+			if !IsArrayContain(title, key) {
+				panic("key[" + key + "] not contain!")
+			}
+		} else {
+			var item = make(map[string]string)
+			for j := range array {
+				item[title[j]] = array[j]
+			}
+			db[item[key]] = item
+		}
+	}
+	return db
+}
+
+func IsArrayContain(array []string, key string) bool {
+	for _, item := range array {
+		if item == key {
+			return true
+		}
+	}
+	return false
+}
