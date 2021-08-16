b77c1f6d9bdc416e0dec841334978851d1687806
Wang Yaoshen <wangyaoshen@bgi.com>
2021-08-16 14:03:58 +0800

    :bug:add return for JoinMapArray() and JoinDb2MapArray()

--- dataframeUtil/dataframe.go
+++ dataframeUtil/dataframe.go
@@ -1,6 +1,6 @@
 package dataframeUtil
 
-func JoinDb2MapArray(left []map[string]string, db map[string]map[string]string, key string) {
+func JoinDb2MapArray(left []map[string]string, db map[string]map[string]string, key string) []map[string]string {
 	for _, item := range left {
 		var add, ok = db[item[key]]
 		if ok {
@@ -9,13 +9,14 @@ func JoinDb2MapArray(left []map[string]string, db map[string]map[string]string, 
 			}
 		}
 	}
+	return left
 }
 
-func JoinMapArray(left, right []map[string]string, lKey, rKey string) {
+func JoinMapArray(left, right []map[string]string, lKey, rKey string) []map[string]string {
 	var db = make(map[string]map[string]string)
 	for _, m := range right {
 		var key = m[rKey]
 		db[key] = m
 	}
-	JoinDb2MapArray(left, db, lKey)
+	return JoinDb2MapArray(left, db, lKey)
 }
