cb60ac1c46bfa306b524d166f6f27d3f94da32e3
Wang Yaoshen <wangyaoshen@zhonghegene.com>
2025-04-24 11:48:14 +0800

    ✨add ShallowCopy and DeepCopyReflect by gpt-4o

--- simpleUtil/util.go
+++ simpleUtil/util.go
@@ -3,6 +3,7 @@ package simpleUtil
 import (
 	"fmt"
 	"log"
+	"reflect"
 	"regexp"
 	"strings"
 )
@@ -158,4 +159,74 @@ func IsArrayContain(array []string, key string) bool {
 		}
 	}
 	return false
+}
+
+// 通用浅拷贝函数
+func ShallowCopy(src any) any {
+	if src == nil {
+		return nil
+	}
+
+	srcVal := reflect.ValueOf(src)
+	srcType := reflect.TypeOf(src)
+
+	// 创建一个新的实例
+	copyVal := reflect.New(srcType.Elem()).Elem()
+
+	// 复制每个字段
+	for i := 0; i < srcVal.Elem().NumField(); i++ {
+		field := srcVal.Elem().Field(i)
+		copyVal.Field(i).Set(field)
+	}
+
+	return copyVal.Addr().Interface()
+}
+
+func DeepCopyReflect(src any) any {
+	srcVal := reflect.ValueOf(src)
+	srcType := reflect.TypeOf(src)
+
+	switch srcVal.Kind() {
+	case reflect.Ptr:
+		// 如果是指针，递归复制指针指向的值
+		if srcVal.IsNil() {
+			return nil
+		}
+		copyVal := reflect.New(srcType.Elem())
+		copyVal.Elem().Set(reflect.ValueOf(DeepCopyReflect(srcVal.Elem().Interface())))
+		return copyVal.Interface()
+	case reflect.Struct:
+		// 如果是结构体，逐字段复制
+		copyVal := reflect.New(srcType).Elem()
+		for i := 0; i < srcVal.NumField(); i++ {
+			field := srcVal.Field(i)
+			if field.CanSet() {
+				copyVal.Field(i).Set(reflect.ValueOf(DeepCopyReflect(field.Interface())))
+			}
+		}
+		return copyVal.Interface()
+	case reflect.Slice:
+		// 如果是切片，复制每个元素
+		if srcVal.IsNil() {
+			return nil
+		}
+		copyVal := reflect.MakeSlice(srcType, srcVal.Len(), srcVal.Cap())
+		for i := 0; i < srcVal.Len(); i++ {
+			copyVal.Index(i).Set(reflect.ValueOf(DeepCopyReflect(srcVal.Index(i).Interface())))
+		}
+		return copyVal.Interface()
+	case reflect.Map:
+		// 如果是映射，复制每个键值对
+		if srcVal.IsNil() {
+			return nil
+		}
+		copyVal := reflect.MakeMap(srcType)
+		for _, key := range srcVal.MapKeys() {
+			copyVal.SetMapIndex(key, reflect.ValueOf(DeepCopyReflect(srcVal.MapIndex(key).Interface())))
+		}
+		return copyVal.Interface()
+	default:
+		// 对于其他类型，直接返回值
+		return src
+	}
 }
