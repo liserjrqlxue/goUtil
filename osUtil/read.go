21e2408daaa9cd98d4196916c2a3c5e36897e1a9
Wang Yaoshen <wangyaoshen@zhonghegene.com>
2025-11-21 11:56:44 +0800

    ✨add osUtil.Bytes2MapArray osUtil.Reader2MapArray

--- osUtil/read.go
+++ osUtil/read.go
@@ -2,6 +2,8 @@ package osUtil
 
 import (
 	"bufio"
+	"bytes"
+	"io"
 	"io/fs"
 	"regexp"
 
@@ -13,7 +15,21 @@ func FS2Array(file fs.File) []string {
 	return scannerUtil.Scanner2Array(scanner)
 }
 
+// FS2MapArray 从 fs.File 读取到 []map[string]string
 func FS2MapArray(file fs.File, sep string, skip *regexp.Regexp) ([]map[string]string, []string) {
 	var scanner = bufio.NewScanner(file)
+	return scannerUtil.Scanner2MapArray(scanner, sep, skip)
+}
+
+// Bytes2MapArray 从字节切片读取到 []map[string]string
+func Bytes2MapArray(data []byte, sep string, skip *regexp.Regexp) ([]map[string]string, []string) {
+	reader := bytes.NewReader(data)
+	scanner := bufio.NewScanner(reader)
+	return scannerUtil.Scanner2MapArray(scanner, sep, skip)
+}
+
+// Reader2MapArray 从 io.Reader 读取到 []map[string]string
+func Reader2MapArray(reader io.Reader, sep string, skip *regexp.Regexp) ([]map[string]string, []string) {
+	scanner := bufio.NewScanner(reader)
 	return scannerUtil.Scanner2MapArray(scanner, sep, skip)
 }
